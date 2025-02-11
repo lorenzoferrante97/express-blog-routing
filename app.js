18d4927e9462ffc338fd9bc4fdb9616f6ea20481
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-11 11:47:49 +0100

    add: express setup

---
+++ app.js
@@ -0,0 +1,16 @@
+// express
+
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Server root');
+
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
