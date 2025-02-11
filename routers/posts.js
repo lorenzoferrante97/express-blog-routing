6a408669480037828cef554111c5e4e9dcb26b0a
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-11 12:23:10 +0100

    add: res.send on routes

--- routers/posts.js
+++ routers/posts.js
@@ -6,42 +6,42 @@ const router = express.Router();
 // READ -> index
 router.get('/', (req, res) => {
 
-
+    res.send("Lista di tutti i post");
 
 })
 
 // READ -> show
 router.get('/:id', (req, res) => {
 
-
+    res.send(`Estrazione del post con id ${req.params.id}`);
 
 })
 
 // CREATE -> store
 router.post('/', (req, res) => {
 
-
+    res.send("Creazione di un post");
 
 })
 
 // UPDATE -> update
 router.put('/:id', (req, res) => {
 
-
+    res.send(`Aggiornamento di tutte le info del post con id ${req.params.id}`);
 
 })
 
 // UPDATE -> modify
 router.patch('/:id', (req, res) => {
 
-
+    res.send(`Aggiornamento di una singola info del post con id ${req.params.id}`);
 
 })
 
 // DELETE -> destroy
 router.delete('/:id', (req, res) => {
 
-
+    res.send(`Eliminazione del post con id ${req.params.id}`);
 
 })
 
