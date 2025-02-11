d4e50dbe2c954cea4e0cf91a64d2ac86baefffa6
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-11 12:54:12 +0100

    bonus: route bacheca with posts list

--- routers/posts.js
+++ routers/posts.js
@@ -3,6 +3,48 @@
 import express from 'express';
 const router = express.Router();
 
+// route bacheca per elenco post
+router.get('/bacheca', (req, res) => {
+
+    let postsList = [
+
+        {
+            titolo: "Ricetta 1",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "imgs/ciambellone.jpeg",
+            tags: ["ricetta", "dolce", "ciambellone"]
+        },
+        {
+            titolo: "Ricetta 2",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "imgs/cracker_barbabietola.jpeg",
+            tags: ["ricetta", "salato", "barbabietola"]
+        },
+        {
+            titolo: "Ricetta 3",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "imgs/pane_fritto_dolce.jpeg",
+            tags: ["ricetta", "dolce", "pane"]
+        },
+        {
+            titolo: "Ricetta 4",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "imgs/pasta_barbabietola.jpeg",
+            tags: ["ricetta", "pasta", "barbabietola"]
+        },
+        {
+            titolo: "Ricetta 5",
+            contenuto: "Descrizione della ricetta, ingredienti, tempi di preparazione e come eseguirla con i relativi passaggi",
+            immagine: "imgs/torta_paesana.jpeg",
+            tags: ["ricetta", "dolce", "torta"]
+        },
+    ];
+
+    res.json(postsList);
+
+})
+
+
 // READ -> index
 router.get('/', (req, res) => {
 
