cde91e5587d5766855ab80470b59d4e81618cced
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-11 12:02:09 +0100

    read show route

--- routers/posts.js
+++ routers/posts.js
@@ -3,7 +3,15 @@
 import express from 'express';
 const router = express.Router();
 
+// READ -> index
 router.get('/', (req, res) => {
+
+
+
+})
+
+// READ -> show
+router.get('/:id', (req, res) => {
 
 
 
