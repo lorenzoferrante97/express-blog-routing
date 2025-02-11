dbc53b6b5b10c352737f716652dc97d85dae5186
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-11 12:13:01 +0100

    add: import postsRouter

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+//import router
+import postsRouter from './routers/posts.js';
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
